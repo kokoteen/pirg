35d0d55e85b0bc40ccd2e24e64e00406210a763d
kokoteen <milosmilosevic994@gmail.com>
2023-09-11 17:40:42 +0200

    fix install command
    - overide __str__
    - fix bug with install command
    - add versions for install_requires

--- pirc/models.py
+++ pirc/models.py
@@ -6,8 +6,11 @@ class Package:
     name: str
     version: str = None
 
-    def __eq__(self, other):
+    def __eq__(self, other) -> bool:
         return self.name == other.name
 
-    def __hash__(self):
+    def __hash__(self) -> int:
         return hash(self.name)
+
+    def __str__(self) -> str:
+        return f"{self.name}=={self.version}"
--- setup.py
+++ setup.py
@@ -1,16 +1,14 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="pric",
+    name="pirc",
     version="0.0.1",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
-            "pric = pric.pric:main",
+            "pirc = pirc.pirc:main",
         ],
     },
-    install_requires=[
-        "typer",
-    ],
+    install_requires=["typer==0.9.0", "requests==2.31.0", "packaging==23.1"],
     python_requires=">=3.8",
 )
