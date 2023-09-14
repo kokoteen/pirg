12738095d1e897a2d62568d88db359260a663025
kokoteen <milosmilosevic994@gmail.com>
2023-09-14 16:07:02 +0200

    fix setup

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(
     name="pirc",
     version="0.0.1",
     packages=find_packages(),
+    description="command-line that simplifies the management of project's `requirements.txt` file",
+    long_description=open('README.txt').read(),
     entry_points={
         "console_scripts": [
             "pirc = pirc.pirc:main",
