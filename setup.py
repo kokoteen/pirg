cdac6acc4014e82ab8977831a940429bf88361cb
kokoteen <36460995+kokoteen@users.noreply.github.com>
2023-09-14 16:09:04 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.0.1",
     packages=find_packages(),
     description="command-line that simplifies the management of project's `requirements.txt` file",
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
     entry_points={
         "console_scripts": [
             "pirc = pirc.pirc:main",
