9d35d5a9a76ba5bf025abbe4eefb19bba0892666
kokoteen <milosmilosevic994@gmail.com>
2023-10-12 17:27:43 +0200

    add update all option
    change parse_name -> parse_package_name
    add WrongPkgName exception
    add suffix and version specifiers in package model

--- pirg/pirg.py
+++ pirg/pirg.py
@@ -6,7 +6,7 @@ import traceback
 from typing import List, Optional
 from typing_extensions import Annotated
 import typer
-from .custom_exceptions import NothingToDo, DisabledPipFlag
+from .custom_exceptions import NothingToDo, DisabledPipFlag, WrongPkgName
 from .models import Package
 from .utils import (
     check_for_pip_args,
@@ -15,7 +15,7 @@ from .utils import (
     decorative_print,
     create_requirements,
     get_name_version,
-    parse_name,
+    parse_package_name,
     run_subprocess,
 )
 
@@ -27,6 +27,7 @@ main = typer.Typer()
 def install(
     package_names: Annotated[Optional[List[str]], typer.Argument(help="List of packages")] = None,
     requirements_path: Annotated[str, typer.Option()] = find_requirements_file(),
+    update_all: Annotated[bool, typer.Option()] = False,
 ) -> None:
     """
     Installs [package_names] and puts them in the requirements file on [requirements_path] location
@@ -37,27 +38,33 @@ def install(
         `pirg install torch -- --index-url https://download.pytorch.org/whl/cu118`
 
     """
-    pip_args = check_for_pip_args()
-    package_names = set(package_names) - pip_args
-
     try:
+        pip_args = check_for_pip_args()
+        package_names = set(package_names) - pip_args
+
         current_pkgs = load_requirements_file(requirements_loc=requirements_path)
         new_pkgs = {get_name_version(package_name=pkg) for pkg in package_names}
-        new_pkgs = new_pkgs - current_pkgs
+        new_pkgs = new_pkgs - current_pkgs  # check if pkg is already in req
 
-        # fmt: off
-        # check for update
-        all_req_pkgs = {c for c in current_pkgs for n in new_pkgs if c.name != n.name}
-        all_req_pkgs.update(new_pkgs)
-        create_requirements(package_names=all_req_pkgs, requirements_loc=requirements_path)
-        # fmt: on
+        if update_all:
+            # all pkgs update
+            update_current_pkgs = {get_name_version(package_name=pkg.name) for pkg in current_pkgs}
+            update_current_pkgs = new_pkgs.union(update_current_pkgs)
+            new_pkgs = update_current_pkgs
+        else:
+            # one pkg update
+            # check if it has same name but different version
+            update_current_pkgs = {c for c in current_pkgs for n in new_pkgs if c.name != n.name}
+            update_current_pkgs.update(new_pkgs)
 
         ins_pkgs = [f"{p.name}=={p.version}" for p in new_pkgs]
 
-        if not ins_pkgs and not pip_args:
+        skip_pip_args = {"-h", "--help"}
+        if not ins_pkgs and not update_all and not bool(skip_pip_args & pip_args):
             raise NothingToDo("Nothing to install")
 
         run_subprocess(pkgs=ins_pkgs, pip_command="install", pip_args=list(pip_args))
+        create_requirements(package_names=update_current_pkgs, requirements_loc=requirements_path)
     except HTTPError as e:
         pkg_name = e.args[0].split()[-1].split("/")[-2]
         decorative_print(f"Failed to find the latest version of {pkg_name} on PyPI")
@@ -67,7 +74,7 @@ def install(
         decorative_print("Failed to install packages")
         traceback.print_exc()
         sys.exit(e.returncode)
-    except NothingToDo as e:
+    except (NothingToDo, DisabledPipFlag, WrongPkgName) as e:
         decorative_print(str(e))
         sys.exit(e.exit_code)
 
@@ -84,16 +91,21 @@ def uninstall(
     You can pass additional `pip uninstall` arguments after "--".
 
     Example:
-        `pirg uninstall torch -- -r requirements.txt --yes`
+        `pirg uninstall torch -- --yes`
 
     """
     try:
         pip_args = check_for_pip_args()
         package_names = set(package_names) - pip_args
+        print(package_names)
 
-        package_names = [parse_name(val) for val in package_names]
-        new_pkgs = set([Package(name, version) for name, version in package_names])
+        package_names = [parse_package_name(val) for val in package_names]
+        new_pkgs = set(
+            [Package(name=name, version=version) for name, _, _, version in package_names]
+        )
+        print(new_pkgs)
         current_pkgs = load_requirements_file(requirements_loc=requirements_path)
+        print(current_pkgs)
 
         if delete_all:
             new_pkgs.update(current_pkgs)
@@ -101,7 +113,7 @@ def uninstall(
         # repopulate version from requirements.txt
         new_pkgs = {c for c in current_pkgs for n in new_pkgs if c.name == n.name}
         current_pkgs = current_pkgs - new_pkgs
-        create_requirements(package_names=current_pkgs, requirements_loc=requirements_path)
+        print(current_pkgs)
 
         rm_pkgs = [p.name for p in new_pkgs]
 
@@ -110,6 +122,7 @@ def uninstall(
             raise NothingToDo("Nothing to remove")
 
         run_subprocess(pkgs=rm_pkgs, pip_command="uninstall", pip_args=list(pip_args))
+        create_requirements(package_names=current_pkgs, requirements_loc=requirements_path)
     except HTTPError as e:
         pkg_name = e.args[0].split()[-1].split("/")[-2]
         decorative_print(f"Failed to find the latest version of {pkg_name} on PyPI")
@@ -119,10 +132,7 @@ def uninstall(
         decorative_print("Failed to remove packages")
         traceback.print_exc()
         sys.exit(e.returncode)
-    except NothingToDo as e:
-        decorative_print(str(e))
-        sys.exit(e.exit_code)
-    except DisabledPipFlag as e:
+    except (NothingToDo, DisabledPipFlag, WrongPkgName) as e:
         decorative_print(str(e))
         sys.exit(e.exit_code)
 
