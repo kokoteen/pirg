f6509633f98de044613f2eb7955e6a61b74fe5f8
kokoteen <milosmilosevic994@gmail.com>
2023-10-21 02:23:50 +0200

    up version and add deps

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="pirg",
-    version="1.2.0",
+    version="1.3.0",
     packages=find_packages(),
     description="command-line tool that simplifies the management of project's `requirements.txt` file",
     long_description_content_type="text/markdown",
@@ -16,6 +16,12 @@ setup(
             "pirg = pirg.pirg:main",
         ],
     },
-    install_requires=["typer[all]==0.9.0", "requests==2.31.0", "packaging==23.2"],
+    install_requires=[
+        "typer[all]==0.9.0",
+        "requests==2.31.0",
+        "packaging==23.2",
+        "beautifulsoup4==4.12.2",
+        "fuzzywuzzy[speedup]=0.9.0",
+    ],
     python_requires=">=3.8",
 )
